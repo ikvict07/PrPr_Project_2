3fed11d6b61baac2cc9dce513687dd3905dedae5
ikvict <antongorobec101@gmail.com>
2023-11-29 01:22:35 +0100

    Refactored version with edge-cases being handled and input validation

--- main.c
+++ main.c
@@ -7,10 +7,12 @@ typedef struct {
     int numbering;
     char type_id;
 } ID;
+
 typedef struct {
     double latitude;
     double longitude;
 } Position;
+
 typedef struct {
     ID id;
     Position position;
@@ -19,25 +21,27 @@ typedef struct {
     char time[5];
     char date[9];
 } Module;
+
 typedef struct ModuleLinkedList {
     Module module;
-    struct ModuleLinkedList *next;
+    struct ModuleLinkedList* next;
 } ModuleLinkedList;
 
 
-void deleteList(ModuleLinkedList **list) {
-    ModuleLinkedList *current = *list;
-    ModuleLinkedList *next;
+void deleteList(ModuleLinkedList** list) {
+    ModuleLinkedList* current = *list;
+
 
     while (current != NULL) {
-        next = current->next;
+        ModuleLinkedList* next = current->next;
         free(current);
         current = next;
     }
 
     *list = NULL;
 }
-int compare(char *date1, char *time1, char *date2, char *time2) {
+
+int compare(const char* date1, const char* time1, const char* date2, const char* time2) {
     int dateA, dateB;
     sscanf(date1, "%d", &dateA);
     sscanf(date2, "%d", &dateB);
@@ -51,14 +55,16 @@ int compare(char *date1, char *time1, char *date2, char *time2) {
     return diff; // IF date1 is bigger => returns positive number
 }
 
-void case_v(ModuleLinkedList *list){
+void case_v(ModuleLinkedList* list) {
     if (list == NULL) {
+        printf("Zoznam je prazdny!\n");
         return;
     }
     int counter = 1;
     do {
         printf("%d:\n", counter);
-        printf("ID: %c%d%c\t%s\t%.2lf\n", list->module.id.designation, list->module.id.numbering, list->module.id.type_id, list->module.type, list->module.value);
+        printf("ID: %c%d%c\t%s\t%.2lf\n", list->module.id.designation, list->module.id.numbering,
+               list->module.id.type_id, list->module.type, list->module.value);
         printf("Poz: %c%.4lf\t%c%.4lf\n",
                list->module.position.latitude > 0 ? '+' : '-', list->module.position.latitude,
                list->module.position.longitude > 0 ? '+' : '-', list->module.position.longitude);
@@ -67,15 +73,14 @@ void case_v(ModuleLinkedList *list){
     }
     while (list->next != NULL && (list = list->next)); // We want to take next only if list has a next element.
     // Logical AND is lazy in C, so it will not evaluate the second part if the first part is false.
-
 }
-void case_n(ModuleLinkedList **list) {
+
+void case_n(ModuleLinkedList** list) {
     if (*list != NULL) {
         deleteList(list);
     }
 
-    FILE *main_file = fopen( "dataloger_V2.txt", "r");
-    rewind(main_file);
+    FILE* main_file = fopen("dataloger_V2.txt", "r");
     if (main_file == NULL) {
         printf("Zaznamy neboli nacitane!\n");
         return;
@@ -93,20 +98,20 @@ void case_n(ModuleLinkedList **list) {
     char date[9];
 
     char latitudeStr[9], longitudeStr[9];
-    while(fscanf(main_file, "%3s\n%c%3d%c\n%8s%8s\n%2s\n%lf\n%s\n%s",
-           _,
-           &designation,
-           &numbering,
-           &type_id,
-           latitudeStr,
-           longitudeStr,
-           type,
-           &value,
-           time,
-           date) == 10) {
+    while (fscanf(main_file, "%3s\n%c%3d%c\n%8s%8s\n%2s\n%lf\n%s\n%s",
+                  _,
+                  &designation,
+                  &numbering,
+                  &type_id,
+                  latitudeStr,
+                  longitudeStr,
+                  type,
+                  &value,
+                  time,
+                  date) == 10) {
         counter++;
 
-        ModuleLinkedList *new_module = (ModuleLinkedList *) malloc(sizeof(ModuleLinkedList));
+        ModuleLinkedList* new_module = malloc(sizeof(ModuleLinkedList));
         new_module->module.id.designation = designation;
         new_module->module.id.numbering = numbering;
         new_module->module.id.type_id = type_id;
@@ -119,8 +124,9 @@ void case_n(ModuleLinkedList **list) {
         new_module->next = NULL;
         if (*list == NULL) {
             *list = new_module;
-        } else {
-            ModuleLinkedList *temp = *list;
+        }
+        else {
+            ModuleLinkedList* temp = *list;
             while (temp->next != NULL) {
                 temp = temp->next;
             }
@@ -128,11 +134,16 @@ void case_n(ModuleLinkedList **list) {
         }
     }
     printf("Nacitalo sa %d zaznamov\n", counter);
+    fclose(main_file);
 }
-void case_p(ModuleLinkedList **list) {
 
+void case_p(ModuleLinkedList** list) {
     int insert_position;
-    scanf("%d", &insert_position);
+    int const scanResult = scanf("%d", &insert_position);
+    if (scanResult < 1 || insert_position < 1) {
+        // printf("Invalid input. Please enter a number.\n"); // The task says we cannot print anything
+        return;
+    }
     char id[6];
     char designation;
     int numbering;
@@ -151,7 +162,17 @@ void case_p(ModuleLinkedList **list) {
     scanf("%s", time);
     scanf("%s", date);
 
-    ModuleLinkedList *new_node = (ModuleLinkedList *) malloc(sizeof(ModuleLinkedList));
+    if (designation < 'A' || designation > 'Z' || numbering < 100 || numbering > 999 ||
+        type_id < 'a' || type_id > 'z' || value < 0 || value > 999.99) {
+        printf("Invalid input!\n"); // The task says we cannot print anything
+        return;
+    }
+
+    ModuleLinkedList* new_node = malloc(sizeof(ModuleLinkedList));
+    if (new_node == NULL) {
+        // printf("Alloc error\n"); // The task says we cannot print anything
+        exit(1);
+    }
     new_node->module.id.designation = designation;
     new_node->module.id.numbering = numbering;
     new_node->module.id.type_id = type_id;
@@ -163,6 +184,10 @@ void case_p(ModuleLinkedList **list) {
     strcpy(new_node->module.date, date);
     new_node->next = NULL;
 
+    if (*list == NULL) {
+        *list = new_node;
+        return;
+    }
     if (insert_position == 1) {
         new_node->next = *list;
         *list = new_node;
@@ -170,7 +195,7 @@ void case_p(ModuleLinkedList **list) {
     }
 
     int counter = 1;
-    ModuleLinkedList *current_node = *list;
+    ModuleLinkedList* current_node = *list;
 
     while (current_node != NULL && counter < insert_position - 1) {
         current_node = current_node->next;
@@ -179,8 +204,9 @@ void case_p(ModuleLinkedList **list) {
     if (current_node != NULL) {
         new_node->next = current_node->next;
         current_node->next = new_node;
-    } else {
-        ModuleLinkedList *last_node = *list;
+    }
+    else {
+        ModuleLinkedList* last_node = *list;
         while (last_node->next != NULL) {
             last_node = last_node->next;
         }
@@ -188,47 +214,64 @@ void case_p(ModuleLinkedList **list) {
         new_node->next = NULL;
     }
 }
-void case_z(ModuleLinkedList **list) {
+
+void case_z(ModuleLinkedList** list) {
+    if (*list == NULL) {
+        printf("Zoznam je prazdny!\n");
+        return;
+    }
     char id[6];
     char designation;
     int numbering;
     char type_id;
     scanf("%6s", id);
     sscanf(id, "%c%3d%c", &designation, &numbering, &type_id);
-    while((*list)->next != NULL) {
-        while ((*list)->module.id.designation == designation &&
-            (*list)->module.id.numbering == numbering &&
-            (*list)->module.id.type_id == type_id) {
-            ModuleLinkedList *temp = *list;
-            *list = (*list)->next;
+    ModuleLinkedList* prev = NULL;
+    ModuleLinkedList* current = *list;
+
+    while (current != NULL) {
+        if (current->module.id.designation == designation &&
+            current->module.id.numbering == numbering &&
+            current->module.id.type_id == type_id) {
+            ModuleLinkedList* temp = current;
+            if (prev == NULL) {
+                *list = current->next;
+            }
+            else {
+                prev->next = current->next;
+            }
+            current = current->next;
             free(temp);
+            printf("Zaznam pre ID: %c%d%c bol vymazany\n", designation, numbering, type_id);
         }
-        list = &(*list)->next;
+        else {
+            prev = current;
+            current = current->next;
+        }
     }
 }
-void swap(ModuleLinkedList *a, ModuleLinkedList *b)
-{
-    Module temp = a->module;
+
+void swap(ModuleLinkedList* a, ModuleLinkedList* b) {
+    const Module temp = a->module;
     a->module = b->module;
     b->module = temp;
 }
-void bubbleSort(ModuleLinkedList **list)
-{
+
+void bubbleSort(ModuleLinkedList** list) {
     int swapped;
-    ModuleLinkedList *current;
-    ModuleLinkedList *temp = NULL;
 
-    if(list == NULL)
+    ModuleLinkedList const* temp = NULL;
+
+    if (list == NULL)
         return;
-    do
-    {
+    do {
         swapped = 0;
-        current = *list;
+        ModuleLinkedList* current = *list;
 
-        while(current->next != temp) //Sorts until the right border
+        while (current->next != temp) //Sorts until the right border
         {
-            if(compare(current->module.date, current->module.time, current->next->module.date, current->next->module.time) > 0)
-            {
+            if (compare(current->module.date, current->module.time, current->next->module.date,
+                        current->next->module.time) > 0) {
                 swap(current, current->next);
                 swapped = 1;
             }
@@ -236,33 +279,48 @@ void bubbleSort(ModuleLinkedList **list)
         }
 
         temp = current; //Right border
-    } while(swapped);
+    }
+    while (swapped);
 }
 
-void case_u(ModuleLinkedList **list) {
+void case_u(ModuleLinkedList** list) {
+    if (*list == NULL) {
+        printf("Chyba usporiadania!\n");
+        return;
+    }
     bubbleSort(list);
     printf("Spajany zoznam bol usporiadany\n");
 }
-void case_r(ModuleLinkedList **list) {
-    ModuleLinkedList *first_node = *list;
-    ModuleLinkedList *second_node = *list;
+
+void case_r(ModuleLinkedList** list) {
+    ModuleLinkedList* first_node = *list;
+    ModuleLinkedList* second_node = *list;
     int first_position, second_position;
-    scanf("%d %d", &first_position, &second_position);
+    int const scanResult = scanf("%d %d", &first_position, &second_position);
+    if (scanResult < 2) {
+        return;
+    }
+    if (*list == NULL) {
+        return;
+    }
     int counter = 1;
-    while (counter < first_position) {
+    while (counter < first_position && first_node != NULL) {
         counter++;
         first_node = first_node->next;
     }
-    while(counter < second_position) {
+    while (counter < second_position && second_node != NULL) {
         counter++;
         second_node = second_node->next;
+    }
+    if (first_node == NULL || second_node == NULL) {
+        return;
     }
     swap(first_node, second_node);
 }
 
 int main() {
     char what_to_do;
-    ModuleLinkedList *list = NULL;
+    ModuleLinkedList* list = NULL;
     while (1) {
         scanf(" %c", &what_to_do);
         switch (what_to_do) {
@@ -273,7 +331,6 @@ int main() {
                 case_n(&list);
                 break;
             case 'p':
-                printf("Inserting new module\n");
                 case_p(&list);
                 break;
             case 'z':
@@ -287,14 +344,14 @@ int main() {
                 break;
             case 'k':
                 while (list != NULL) {
-                    ModuleLinkedList *temp = list;
+                    ModuleLinkedList* temp = list;
                     list = list->next;
                     free(temp);
                 }
-                return 0;
+                exit(EXIT_SUCCESS);
             default:
                 printf("Invalid input\n");
-                return 0;
+                break;
         }
     }
 }
